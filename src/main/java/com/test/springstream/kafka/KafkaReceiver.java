324d98c3376c44de55d128f54f68633e3cf35ddd
mustangkong <mustang2247@gmail.com>
2017-11-06 23:27:51 +0800

    update code

--- src/main/java/com/test/springstream/kafka/KafkaReceiver.java
+++ src/main/java/com/test/springstream/kafka/KafkaReceiver.java
@@ -1,10 +1,11 @@
 package com.test.springstream.kafka;
 
-import com.test.springstream.kafka.input.Sink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
 
 /**
  * 消费者端代码
@@ -16,7 +17,7 @@ public class KafkaReceiver {
 
     private final Logger logger = LoggerFactory.getLogger(KafkaReceiver.class);
 
-    @StreamListener(Sink.INPUT)
+    @StreamListener(Processor.OUTPUT)
     private void receive(String vote) {
         logger.info("receive message : " + vote);
     }
--- src/main/java/com/test/springstream/kafka/KafkaSendService.java
+++ src/main/java/com/test/springstream/kafka/KafkaSendService.java
@@ -1,10 +1,11 @@
 package com.test.springstream.kafka;
 
-import com.test.springstream.kafka.source.Source;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.support.MessageBuilder;
 
 /**
@@ -13,7 +14,7 @@ import org.springframework.integration.support.MessageBuilder;
  * 负责消息发送
  */
 @EnableBinding(Source.class)
-public class KafkaSendService {
+public class KafkaSendService implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(KafkaSendService.class);
 
@@ -22,16 +23,16 @@ public class KafkaSendService {
 
     public void sendMessage(String message) {
         try {
-            source.output1().send(MessageBuilder.withPayload(message).build());
+            source.output().send(MessageBuilder.withPayload(message).build());
         } catch (Exception e) {
             logger.info("消息发送失败，原因："+e);
             e.printStackTrace();
         }
     }
 
-//    public void run(String... strings) throws Exception {
-//        // 字符串类型发送MQ
-//        System.out.println("字符串信息发送");
-//        sendMessage("字符串信息发送");
-//    }
+    public void run(String... strings) throws Exception {
+        // 字符串类型发送MQ
+        System.out.println("字符串信息发送");
+        sendMessage("asdfasdfasdfasdfasdfasdf");
+    }
 }
